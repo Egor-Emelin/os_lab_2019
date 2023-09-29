325c41dcd54095dd53eb131a060d9bbc43a6de1d
Egor <129104442+Egor-Emelin@users.noreply.github.com>
2023-09-29 18:46:40 +0000

    добавлен revert_string.c и swap.c.

--- lab2/src/revert_string/revert_string.c
+++ lab2/src/revert_string/revert_string.c
@@ -1,7 +1,17 @@
 #include "revert_string.h"
+#include <string.h>
 
 void RevertString(char *str)
 {
-	// your code here
+	int length = strlen(str);
+    int i, j;
+    char temp;
+
+    // Проходим половину строки и меняем символы местами
+    for (i = 0, j = length - 1; i < j; ++i, --j) {
+        temp = str[i];
+        str[i] = str[j];
+        str[j] = temp;
+    }
 }
 
